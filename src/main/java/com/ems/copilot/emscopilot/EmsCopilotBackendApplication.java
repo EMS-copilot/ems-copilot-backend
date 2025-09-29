cf4cabc2172d9a7acb59286433411fc461c95ae7
BaeSJ1 <bsj92@example.com>
2025-09-29 12:50:14 +0900

    chore: TypeScript 프로젝트를 Spring Boot 프로젝트로 전환

---
+++ src/main/java/com/ems/copilot/emscopilot/EmsCopilotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ems.copilot.emscopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsCopilotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsCopilotBackendApplication.class, args);
+	}
+
+}
