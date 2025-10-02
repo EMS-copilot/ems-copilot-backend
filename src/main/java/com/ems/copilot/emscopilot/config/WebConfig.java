c562e6fb1e1efff9d92d957c970e35bc2d0918c5
BaeSJ1 <bsj92@example.com>
2025-10-02 18:47:14 +0900

    feat: Cors 설정 파일 추가

---
+++ src/main/java/com/ems/copilot/emscopilot/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.ems.copilot.emscopilot.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
