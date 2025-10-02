f8f5a85ac0bd5bf97eb297e41e29e6a8a978e06f
BaeSJ1 <bsj92@example.com>
2025-10-02 19:47:37 +0900

    feat: ApiResponse DTO 추가

---
+++ src/main/java/com/ems/copilot/emscopilot/dto/response/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.ems.copilot.emscopilot.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse <T> {
+    private String status;
+    private String message;
+    private T data;
+}
