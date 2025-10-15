d9a4a37295538f0b95cf0ec14506905224db129e
BaeSJ1 <bsj92@example.com>
2025-10-15 22:41:55 +0900

    feat: 접근 권한이 없을 때 ErrorCode 추가

--- src/main/java/com/ems/copilot/emscopilot/exception/ErrorCode.java
+++ src/main/java/com/ems/copilot/emscopilot/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "사용자를 찾을 수 없습니다."),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "U002", "비밀번호가 일치하지 않습니다.");
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "U002", "비밀번호가 일치하지 않습니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "U003", "접근 권한이 없습니다.");
 
     private final HttpStatus status;
     private final String code;
